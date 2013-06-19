11ebe44be0568b41555004a34ef071665a28173e
Tanay Tayal <tanay@mac.com>
2013-06-19 15:46:56 +0530

    Resizing tiles and balloons on display

--- MatchThree/MatchThree/Classes/constants.h
+++ MatchThree/MatchThree/Classes/constants.h
@@ -9,14 +9,14 @@
 #ifndef MatchThree_constants_h
 #define MatchThree_constants_h
 
-#define kTileSize 65.0f
-#define kPieceHeight 55.0f
-#define kPieceWidth 55.0f
+#define kTileSize 78.0f
+#define kPieceHeight 68.0f
+#define kPieceWidth 68.0f
 #define kMoveTileTime 0.3f
 #define kBoxWidth 9
 #define kBoxHeight 9
-#define kStartX 200
-#define kStartY 60
+#define kStartX 280
+#define kStartY 25
 #define kMaxLevelNo 10
 #define kMaxRecordCount 5
 #define kKindCount 6
